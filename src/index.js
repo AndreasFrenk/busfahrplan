3e4e5272a6acd471bb391c0b2eb0d37e10255639
kryptoguy <andyfrenk@web.de>
2020-12-24 10:02:11 +0100

    initial project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import L from "leaflet";
+import { render } from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// https://cherniavskii.com/using-leaflet-in-react-apps/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const mapStyle = {
+  width: "100%",
+  height: "90vh"
+};
+
+class App extends React.Component {
+  map
+  fourd
+  groups
+  project
+  plottable
+  leafletHelper
+
+  constructor(props){
+    super(props);
+
+  }
+
+
+  getData() {
+    // create a new XMLHttpRequest
+    var xhr = new XMLHttpRequest()
+
+    // get a callback when the server responds
+    xhr.addEventListener('load', () => {
+      // update the state of the component with the result here
+      console.log(xhr.responseText)
+    })
+    // open the request with the verb and the url
+    xhr.open('GET', 'https://rest.busradar.conterra.de/prod/haltestellen')
+    // send the request
+    xhr.send()
+  }
+
+
+  /**
+   * Initialise the Leaflet map
+   */
+  componentDidMount() {
+    this.getData();
+
+
+    // Create map
+    this.map = L.map("map", {
+      center: [51.9606649, 7.6261347],
+      zoom: 15,
+      layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+          attribution: 'Someone'
+        })
+      ]
+    });
+  }
+
+  /**
+   * Add or remove tile layer
+   * Note you will have to move camera on callback manually (if you want), as shown in this example
+   */
+
+  render() {
+    return (
+      <div>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+          crossorigin=""/>
+        <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
+          integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
+          crossorigin=""></script>
+        <div id="map" style={mapStyle} />
+
+      </div>);
+  }
+}
+
+render(<App />, document.getElementById('root'));
+
 reportWebVitals();
